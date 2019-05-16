b347dec2868bc08a07d79c65f3d75182027ac43a
Sam <samarony.barros@gmail.com>
2019-05-16 12:44:46 +0200

    includes node file

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const apiPort = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(apiPort, () => console.log('Server running on port 3000'))
