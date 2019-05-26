bd8f9a434993d38ce2aeadbc1cd4f4d69a3af7a3
Sam <samarony.barros@gmail.com>
2019-05-26 20:27:58 +0200

    CRUD backend for movies

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
+
 const app = express()
 const apiPort = 3000
 
@@ -8,8 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', movieRouter)
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
